d9d9aaea266ef154748accf6c3c7d4973e76d200
Wei Wei <weiway@outlook.com>
2017-09-08 14:58:52 -0600

    Minor updates

--- src/app/alltimes/alltimes.component.ts
+++ src/app/alltimes/alltimes.component.ts
@@ -13,6 +13,18 @@ export class AlltimesComponent implements OnInit {
     { user: 'Karen', project: 'Agile Times', category: 'Frontend', startTime: 900, endTime: 1700, date: 1434243 },
     { user: 'Si', project: 'Mobile App', category: 'Operations', startTime: 1100, endTime: 1700, date: 1434243 },
     { user: 'Rohit', project: 'Agile Times', category: 'Backend', startTime: 800, endTime: 1700, date: 1434243 },
+    { user: '1Glen', project: 'Payroll App', category: 'Backend', startTime: 1000, endTime: 1700, date: 1434243 },
+    { user: '1Karen', project: 'Agile Times', category: 'Frontend', startTime: 900, endTime: 1700, date: 1434243 },
+    { user: '1Si', project: 'Mobile App', category: 'Operations', startTime: 1100, endTime: 1700, date: 1434243 },
+    { user: '1Rohit', project: 'Agile Times', category: 'Backend', startTime: 800, endTime: 1700, date: 1434243 },
+    { user: '2Glen', project: 'Payroll App', category: 'Backend', startTime: 1000, endTime: 1700, date: 1434243 },
+    { user: '2Karen', project: 'Agile Times', category: 'Frontend', startTime: 900, endTime: 1700, date: 1434243 },
+    { user: '2Si', project: 'Mobile App', category: 'Operations', startTime: 1100, endTime: 1700, date: 1434243 },
+    { user: '2Rohit', project: 'Agile Times', category: 'Backend', startTime: 800, endTime: 1700, date: 1434243 },
+    { user: '3Glen', project: 'Payroll App', category: 'Backend', startTime: 1000, endTime: 1700, date: 1434243 },
+    { user: '3Karen', project: 'Agile Times', category: 'Frontend', startTime: 900, endTime: 1700, date: 1434243 },
+    { user: '3Si', project: 'Mobile App', category: 'Operations', startTime: 1100, endTime: 1700, date: 1434243 },
+    { user: '3Rohit', project: 'Agile Times', category: 'Backend', startTime: 800, endTime: 1700, date: 1434243 },
   ];
 
   allProjectsNames = ['', 'Payroll App', 'Agile Times', 'Mobile App'];
@@ -26,10 +38,6 @@ export class AlltimesComponent implements OnInit {
   recordCount: number;
 
   constructor() { 
-    for(let x = 0; x < 5; x++) {
-      this.allTimesheetData = this.allTimesheetData.concat(this.allTimesheetData);
-    }
-
     this.recordCount = this.allTimesheetData.length;
   }
 
